9ed1952a39f81ad6cb3d43650573f72b11c45486
kanzash <kanzash@github.com>
2017-11-11 14:18:16 -0500

    first commit

---
+++ GPA/src/Course.java
@@ -0,0 +1,164 @@
+public class Course {
+
+    String name;
+    double lecHrs;
+    double labHrs;
+    double credHrs;
+    double percentGrade;
+    double GPE; //grade point equivalency (converted from table)
+    double WGP; //weighted grade points
+
+    public Course() {
+
+        this.credHrs = 0;
+        this.labHrs = 0;
+        this.lecHrs = 0;
+
+    }
+
+    public Course (String name, double lecHrs, double labHrs, double percentGrade) {
+
+        this.name = name;
+        this.lecHrs = lecHrs;
+        this.labHrs = labHrs;
+        this.percentGrade = percentGrade;
+
+        this.credHrs = (lecHrs) + (labHrs/2);
+
+
+
+        this.GPE = GPEConversion(percentGrade);
+
+        this.WGP = this.GPE * this.credHrs;
+    }
+
+    public double GPEConversion(double percentGrade) {
+
+        if(percentGrade >= 0 && percentGrade <= 49) {
+
+            return 0.0;
+        }
+
+        else if(percentGrade >= 50 && percentGrade <= 52) {
+
+            return 0.7;
+        }
+
+        else if(percentGrade >= 53 && percentGrade <= 56) {
+
+            return 1.0;
+        }
+
+        else if(percentGrade >= 57 && percentGrade <= 59) {
+
+            return 1.3;
+        }
+
+        else if(percentGrade >= 60 && percentGrade <= 62) {
+
+            return 1.7;
+        }
+
+        else if(percentGrade >= 63 && percentGrade <= 66) {
+
+            return 2.0;
+        }
+
+        else if(percentGrade >= 67 && percentGrade <= 69) {
+
+            return 2.3;
+        }
+
+        else if(percentGrade >= 70 && percentGrade <= 72) {
+
+            return 2.7;
+        }
+
+        else if(percentGrade >= 73 && percentGrade <= 76) {
+
+            return 3.0;
+        }
+
+        else if(percentGrade >= 77 && percentGrade <= 79) {
+
+            return 3.3;
+        }
+
+        else if(percentGrade >= 80 && percentGrade <= 84) {
+
+            return 3.7;
+        }
+
+        else if(percentGrade >= 85 && percentGrade <= 100) {
+
+            return 4.0;
+        }
+
+        return 0;
+    }
+
+
+
+    public double getLecHrs() {
+        return lecHrs;
+    }
+
+    public double getLabHrs() {
+        return labHrs;
+    }
+
+    public double getCredHrs() {
+        return credHrs;
+    }
+
+    public double getGPE() {
+        return GPE;
+    }
+
+    public double getPercentGrade() {
+        return percentGrade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWGP() {
+        return WGP;
+    }
+
+    public void setCredHrs(double credHrs) {
+        this.credHrs = credHrs;
+    }
+
+    public void setLabHrs(double labHrs) {
+        this.labHrs = labHrs;
+    }
+
+    public void setLecHrs(double lecHrs) {
+        this.lecHrs = lecHrs;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public void setGPE(double GPE) {
+        this.GPE = GPE;
+    }
+
+    public void setPercentGrade(double percentGrade) {
+        this.percentGrade = percentGrade;
+    }
+
+    public void setWGP(double WGP) {
+        this.WGP = WGP;
+    }
+
+    public String toString(Course c) {
+
+        return "Course: " + c.name + "\n" + "Lecture Hours: " + c.getLecHrs() + "\n" + "Lab Hours: " + c.getLabHrs() + "\n" + "Grade: " + c.getPercentGrade() + "\n" + "Grade Point Equivalency: " + c.GPE + "\n";
+        //return ("Hello");
+    }
+}
---
+++ GPA/src/Main.java
@@ -0,0 +1,130 @@
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    ArrayList<Course> allCourse = new ArrayList<Course>();
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        Main m = new Main();
+        m.UserInterface();
+    }
+
+    public void UserInterface() {
+
+        System.out.println("Welcome to GPA Calculator");
+
+
+
+        Scanner s = new Scanner(System.in);
+
+
+        boolean running = true;
+
+        while(running) {
+
+            System.out.println("1.Enter grades for new course");
+            System.out.println("2.View all courses entered");
+            System.out.println("3.View GPA for all courses entered");
+            System.out.println("4. Quit");
+
+            String input = s.nextLine();
+            input.charAt(1);
+
+            switch (input) {
+
+                case "1":
+                    CourseInfo();
+                    break;
+
+                case "2":
+                    PrintCourseInfo();
+                    break;
+
+                case "3":
+                    TotalGPA();
+                    break;
+
+                case "4":
+                    running = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    private void CourseInfo() {
+
+        Course c = new Course();
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter course name / course code");
+        String courseName = s.nextLine();
+        //c.setName(courseName);
+        System.out.println(courseName);
+
+        System.out.println("Enter number of lecture hours per week");
+        String courseLec = s.nextLine();
+        //c.setLecHrs(Double.valueOf(courseLec));
+        System.out.println(courseLec);
+
+        System.out.println("Enter number of lab hours per week");
+        String courseLab = s.nextLine();
+       // c.setLabHrs(Double.valueOf(courseLab));
+        System.out.println(courseLab);
+
+        System.out.println("Enter grade received in course 0-100");
+        String avg = s.nextLine();
+        //c.setPercentGrade(Double.valueOf(avg));
+        System.out.println(avg);
+
+        //DE courses? seminars vs lectures //labs vs seminars
+
+        Course c1 = new Course(courseName,Double.valueOf(courseLec),Double.valueOf(courseLab), Double.valueOf(avg));
+
+        //allCourse.add(c);
+        allCourse.add(c1);
+
+        //System.out.println(c.toString(c));
+
+        PrintCourseInfo();
+    }
+
+    private void TotalGPA() {
+
+        double totalCreditHrs = 0;
+        double totalWGP = 0;
+        double GPA = 0;
+
+        for(int i = 0; i < allCourse.size(); i++) {
+
+            totalCreditHrs += allCourse.get(i).getCredHrs();
+
+            totalWGP += allCourse.get(i).getWGP();
+        }
+
+        GPA = totalWGP / totalCreditHrs;
+
+        System.out.println("Your GPA is: " + GPA);
+
+
+    }
+
+    private void PrintCourseInfo() {
+
+        Course c = new Course();
+
+        for(int i = 0; i < allCourse.size(); i++) {
+
+            System.out.println( c.toString(allCourse.get(i)) );
+        }
+    }
+
+}
